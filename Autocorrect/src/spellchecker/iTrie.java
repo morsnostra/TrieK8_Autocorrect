28efe649665e91d4ee5ed8262885ad3d2c2dbce6
hannahlrhl <hannahlarissahalim@gmail.com>
2025-06-03 12:07:22 +0700

    komen

---
+++ Autocorrect/src/spellchecker/iTrie.java
@@ -0,0 +1,21 @@
+package spellchecker;
+
+public interface ITrie {
+
+	void add(String word);
+
+	INode find(String word);
+
+	int getWordCount();
+
+	int getNodeCount();
+
+	@Override
+	String toString();
+
+	@Override
+	int hashCode();
+
+	@Override
+	boolean equals(Object o);
+}
