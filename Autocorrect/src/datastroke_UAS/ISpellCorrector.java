8813c682d8ef547f48fd3c49cdb8bd0805efff55
hannahlrhl <hannahlarissahalim@gmail.com>
2025-06-05 02:10:45 +0700

    komen ISpellCorrector.java

--- Autocorrect/src/datastroke_UAS/ISpellCorrector.java
+++ Autocorrect/src/datastroke_UAS/ISpellCorrector.java
@@ -3,8 +3,12 @@ package datastroke_UAS;
 
 import java.io.IOException;
 
+// interface utama buat spell checker, ini yang bakal dipake user
 public interface ISpellCorrector {
+
+   // load dictionary dari file, bisa throw IOException kalo file bermasalah
    void useDictionary(String var1) throws IOException;
 
+   // kasih saran kata yang mirip (fungsi utamanya spell checker)
    String suggestSimilarWord(String var1);
 }
