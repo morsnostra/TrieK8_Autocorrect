e87e5902f96d8f2ea48b5381307f42855de2eca5
hannahlrhl <hannahlarissahalim@gmail.com>
2025-06-05 02:10:19 +0700

    komen INode.java

--- Autocorrect/src/datastroke_UAS/INode.java
+++ Autocorrect/src/datastroke_UAS/INode.java
@@ -1,13 +1,17 @@
 // Source code is decompiled from a .class file using FernFlower decompiler.
 package datastroke_UAS;
 
+// interface buat representasi node di dalam trie
+// setiap node bisa nyimpen berapa kali kata muncul (frekuensi) 
+// dan bisa punya children node
 
 public interface INode {
-    //buat dapat nilai / frekuensi kata pada node
-   int getValue();
+    // buat dapetin berapa kali kata ini muncul (frekuensi) di dictionary  
+    int getValue();
 
-   //nambah nilai / frekuensi kata pada node
-   void incrementValue();
+    // buat nambah counter kata ini (dipanggil tiap ada kata yang sama)
+    void incrementValue();
 
-   INode[] getChildren();
+    // buat dapat array children dari node ini (buat navigasi ke karakter selanjutnya)
+    INode[] getChildren();
 }
